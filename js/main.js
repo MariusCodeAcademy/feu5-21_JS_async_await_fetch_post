e12e11669897360a21bd8f18c29dd154e0347dd1
Marius CA <marius@example.com>
2023-03-07 10:31:24 +0200

    the problem

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 console.log('main.js file was loaded');
 
-const url = '/db/nums.json';
+const urlBase = '/db/nums.json';
 console.log('pries fetch 1');
 
-function getData() {
+function getData(url) {
   const fetchResult = fetch(url)
     .then((resp) => resp.json())
     .then((dataInJs) => {
-      console.log('getData ===', dataInJs);
+      // console.log('getData ===', dataInJs);
       return dataInJs;
     })
     .catch(console.warn);
@@ -17,7 +17,21 @@ function getData() {
 
 console.log('po fetch 2');
 
-const name1 = getData(); // is funkcijos kuri grazina promise, taip reiksmes negalim pasiimti.
+const name1 = getData(urlBase); // is funkcijos kuri grazina promise, taip reiksmes negalim pasiimti.
 console.log('name1 ===', name1); // promise, pending
 
 // kaip pasiimti reikmes is promise?
+getData(urlBase).then((data) => {
+  const name2 = data.name;
+  console.log('name2 ===', name2); // promise, pending
+});
+
+// tikslas gauti duomenis is nums.json ir is colors.json ir tik tada atspausdinti.
+getData('/db/nums.json')
+  .then((numsData) => {
+    console.log('numsData ===', numsData);
+    return getData('/db/colors.json');
+  })
+  .then((colorsData) => {
+    console.log('colorsData ===', colorsData);
+  });
