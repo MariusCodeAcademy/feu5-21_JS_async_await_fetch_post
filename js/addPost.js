f2cc10d7e20cd541be61e3bc6b00ff098ca32114
Marius CA <marius@example.com>
2023-03-07 12:03:28 +0200

    add post uzd

---
+++ js/addPost.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('addPost.js file was loaded');
+
+// add post url = https://dummyjson.com/posts/add
+
+const newPostData = {
+  title: 'This is new Post from me',
+  body: 'This is new Posts body from me',
+};
+
+// mygtuko paspaudimu issiunciam post su fetch
+// ir aprasom sekmes ir nesekmes atvejus
